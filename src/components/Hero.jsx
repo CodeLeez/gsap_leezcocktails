d47c05078e687bf28faaff0c4143e81d18e9b47b
Elizabeth Chigudu <elizabethchigudu@gmail.com>
2026-03-29 23:15:33 +0800

    fixed: hero video pin, nav scroll links, social hover effects

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -43,8 +43,8 @@ const Hero = () => {
             .to('.right-leaf', {y : 200}, 0)
             .to('.left-leaf', { y: -200}, 0)
 
-        const startValue = isMobile ? 'top 50%' : 'center 60%';
-        const endValue = isMobile ? '120% top' : 'bottom top';
+        const startValue = isMobile ? 'top 50%' : 'top top';
+        const endValue = isMobile ? '120% top' : '+=117%';
 
 
 
